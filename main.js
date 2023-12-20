0675207d09d85d7d03e39d0ea3eb78170a92d83f
shrutijoshi14 <shrutijoshi1481995@gmail.com>
2023-12-20 21:32:12 +0530

    Coffee Cafe Website is created by using HTML, CSS, JS

---
+++ main.js
@@ -0,0 +1,42 @@
+// Swiper
+var swiper = new swiper( ".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disabledOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+} );
+
+// Menu Open Close
+let menu = document.querySelector( ".menu-icon" );
+let navbar = document.querySelector( ".navbar" );
+
+menu.onclick = () => {
+    menu.classList.toggle( "move" );
+    navbar.classList.toggle( "open-menu" );
+};
+
+// Close Menu On Scroll
+window.onscroll = () => {
+    menu.classList.toggle( "move" );
+    navbar.classList.toggle( "open-menu" );
+};
+
+// ScrollReveal Animation
+const animate = ScrollReveal( {
+    origin: 'top',
+    distance: '60px',
+    duration: '2500',
+    delay: '400',
+} );
+animate.reveal( ".nav" );
+animate.reveal( ".home-text", { origin: "left" } );
+animate.reveal( ".home-img", { origin: "bottom" } );
+animate.reveal( ".ser-box, .product-box, .team-box, .book-data", {
+    interval: 100,
+} );
